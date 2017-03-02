1367f4c68569796c8709870317c913c751dadb0d
Trent Willis <twillis@linkedin.com>
2017-03-02 15:33:26 -0800

    Add heimdalljs monitoring

--- index.js
+++ index.js
@@ -11,12 +11,46 @@ var chmod = fs.chmodSync;
 var tmpDir = require('os').tmpdir();
 var debug = require('debug')('sync-disk-cache');
 var zlib = require('zlib');
+var heimdall =  require('heimdalljs');
 
 var mode = {
   mode: parseInt('0777', 8)
 };
 
 var CacheEntry = require('./lib/cache-entry');
+var Metric = require('./lib/metric');
+
+if (!heimdall.hasMonitor('sync-disk-cache')) {
+  heimdall.registerMonitor('sync-disk-cache', function SyncDiskCacheSchema() {});
+}
+
+/*
+ * @private
+ *
+ * Defines a function on the given object at the given property name. Wraps
+ * the function with metric recording for heimdalljs.
+ *
+ * @method defineFunction
+ * @param Object obj the object on which to define the function
+ * @param String name the name to use for the function
+ * @param Function fn
+ * @returns Void
+ */
+function defineFunction(obj, name, fn) {
+  obj[name] = function() {
+    var stats = heimdall.statsFor('sync-disk-cache');
+    var metrics = stats[name] = stats[name] || new Metric();
+
+    metrics.start();
+
+    var result = fn.apply(this, arguments);
+
+    metrics.stop();
+
+    return result;
+  };
+}
+
 /*
  * @private
  * @method processFile
@@ -102,13 +136,13 @@ function Cache(key, _) {
  * @returns {Promise} - fulfills when the cache has been cleared
  *                    - rejects when a failured occured during cache clear
  */
-Cache.prototype.clear = function() {
+defineFunction(Cache.prototype, 'clear', function() {
   debug('clear: %s', this.root);
 
   return rimraf(
     path.join(this.root)
   );
-};
+});
 
 /*
  * @public
@@ -118,12 +152,12 @@ Cache.prototype.clear = function() {
  * @return {Promise} - fulfills with either true | false depending if the key was found or not
  *                   - rejects when a failured occured when checking existence of the key
  */
-Cache.prototype.has = function(key) {
+defineFunction(Cache.prototype, 'has', function(key) {
   var filePath = this.pathFor(key);
   debug('has: %s', filePath);
 
   return fs.existsSync(filePath);
-};
+});
 
 /*
  * @public
@@ -133,7 +167,7 @@ Cache.prototype.has = function(key) {
  * @return {Promise} - fulfills with either the cache entry, or a cache miss entry
  *                   - rejects when a failure occured looking retrieving the key
  */
-Cache.prototype.get = function(key) {
+defineFunction(Cache.prototype, 'get', function(key) {
   var filePath = this.pathFor(key);
   debug('get: %s', filePath);
 
@@ -142,7 +176,7 @@ Cache.prototype.get = function(key) {
   } catch(e) {
     return handleENOENT(e);
   }
-};
+});
 
 /*
  * @public
@@ -153,7 +187,7 @@ Cache.prototype.get = function(key) {
  * @returns {Promise#fulfilled} if the value was coõstored as the key
  * @returns {Promise#rejected} when a failure occured persisting the key
  */
-Cache.prototype.set = function(key, value) {
+defineFunction(Cache.prototype, 'set', function(key, value) {
   var filePath = this.pathFor(key);
   debug('set : %s', filePath);
 
@@ -162,7 +196,7 @@ Cache.prototype.set = function(key, value) {
   chmod(filePath, mode.mode);
 
   return filePath;
-};
+});
 
 /*
  * @public
@@ -172,7 +206,7 @@ Cache.prototype.set = function(key, value) {
  * @returns {Promise#fulfilled} if the removal was successful
  * @returns {Promise#rejection} if something went wrong while removing the key
  */
-Cache.prototype.remove = function(key) {
+defineFunction(Cache.prototype, 'remove', function(key) {
   var filePath = this.pathFor(key);
   debug('remove : %s', filePath);
 
@@ -181,7 +215,7 @@ Cache.prototype.remove = function(key) {
   } catch(e) {
     handleENOENT(e);
   }
-};
+});
 
 /*
  * @public
@@ -190,9 +224,9 @@ Cache.prototype.remove = function(key) {
  * @param {String} key the key to generate the final path for
  * @returns the path where the key's value may reside
  */
-Cache.prototype.pathFor = function(key) {
+defineFunction(Cache.prototype, 'pathFor', function(key) {
   return path.join(this.root, key);
-};
+});
 
 /*
  * @public
@@ -201,10 +235,10 @@ Cache.prototype.pathFor = function(key) {
  * @param {String} compressedValue
  * @returns decompressedValue
  */
-Cache.prototype.decompress = function(value) {
+defineFunction(Cache.prototype, 'decompress', function(value) {
   if (!this.compression) { return value; }
   return COMPRESSIONS[this.compression].out(value);
-};
+});
 
 /*
  * @public
@@ -213,9 +247,9 @@ Cache.prototype.decompress = function(value) {
  * @param {String} value
  * @returns compressedValue
  */
-Cache.prototype.compress = function(value) {
+defineFunction(Cache.prototype, 'compress', function(value) {
   if (!this.compression) { return value; }
   return COMPRESSIONS[this.compression].in(value);
-};
+});
 
 module.exports = Cache;
